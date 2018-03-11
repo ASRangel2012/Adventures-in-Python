2aedb843123174e2a01726e1da47f8679d653f1c
ASRangel2012 <ASRAngel2012@outlook.com>
2018-03-11 10:30:13 -0400

    for statement, if else, dictionary

--- lab 7/favorite_languages_AngelSR.py
+++ lab 7/favorite_languages_AngelSR.py
@@ -11,12 +11,16 @@ favorite_languages = {
     'drew' : 'c',
     'edward':'ruby',
     'phil':'python',
+    'aaron':'',
+    'hank':'',
+    'gloria':'',
     }
 
 #Use for loop and define new variable to get key-value pairs 
 for name, language in favorite_languages.items():
     print(name.title() + "'s favorite language is " +
           language.title() + ".")
+
 
 #Use .keys() to receive keys from dictionary. 
 for name in favorite_languages.keys():
@@ -26,3 +30,27 @@ for name in favorite_languages.keys():
 #same way to do what is above with name variable
 for name in favorite_languages:
     print(name.title())
+
+
+#send special message to only friends
+
+friends = ['phil', 'jen']
+for name in favorite_languages:
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() + ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+
+
+#See who's not included(Hasn't taken the favorite language poll)
+
+
+for name, language in sorted(favorite_languages.items()):
+    if language == '':
+        print("Take our poll and let us know what your favorite language is " + name.title() + "!")
+    else:
+        print("Thank you for taking the poll " + name.title() +"." + " \nI noticed your favorite language was " +
+              language.title())
+
+        
